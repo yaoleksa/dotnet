02390b1ec01f933cc717bacdccdb4b9fd2190abb
Oleksii <oleksii.y@peeklogic.com>
2023-01-23 19:13:38 +0200

    stgmc132n

---
+++ APIGetsSFData (1)/Controllers (1)/GetQBCheck.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Interop.QBFC15;
+using System;
+
+namespace APIGetsSFData.Controllers
+{
+    public class GetQBCheck
+    {
+        public static void BuildCheckGet(IMsgSetRequest req) 
+        {
+            ICheckQuery qr = req.AppendCheckQueryRq();
+            qr
+                .ORTxnQuery
+                .TxnFilter
+                .ORDateRangeFilter
+                .TxnDateRangeFilter
+                .ORTxnDateRangeFilter
+                .TxnDateFilter
+                .FromTxnDate
+                .SetValue(DateTime.Today.AddDays(-7));
+        }
+        public static void handleResponse(IMsgSetResponse resp, List<string> exMemos) 
+        {
+            IResponseList resLst = resp.ResponseList;
+            if(resLst == null || resLst.Count == 0)
+            {
+                return;
+            }
+            for(int i = 0; i < resLst.Count; i++)
+            {
+                IResponse res = resLst.GetAt(i);
+                if(res.Detail == null)
+                {
+                    return;
+                }
+                ICheckRetList retLst = (ICheckRetList)res.Detail;
+                if(retLst == null || retLst.Count == 0)
+                {
+                    return;
+                }
+                for(int j = 0; j < retLst.Count; j++)
+                {
+                    if(retLst.GetAt(j) == null)
+                    {
+                        continue;
+                    }
+                    exMemos.Add(retLst.GetAt(j).Memo.GetValue());
+                }
+            }
+        }
+    }
+}
---
+++ APIGetsSFData (1)/Controllers (1)/customerQB (1).cs
@@ -0,0 +1,9 @@
+﻿namespace APIGetsSFData.Controllers
+{
+    public class customerQB
+    {
+        public string fullName { get; set; }
+        public string editSequence { get; set; }
+        public string listId { get; set; }
+    }
+}
