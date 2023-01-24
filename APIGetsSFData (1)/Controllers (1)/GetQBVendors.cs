721ea5a59024d0485c0aaa5ba81a45877305db64
yaoleksa <mryaremchyk@gmail.com>
2023-01-24 17:18:45 +0200

    optimize API

---
+++ APIGetsSFData (1)/Controllers (1)/GetQBVendors.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using Interop.QBFC15;
+using System;
+
+namespace APIGetsSFData.Controllers
+{
+    public class GetQBVendors
+    {
+        public static void buildRequest(IMsgSetRequest rq)
+        {
+            IVendorQuery vendorQuery = rq.AppendVendorQueryRq();
+            vendorQuery
+                .ORVendorListQuery
+                .VendorListFilter
+                .ORNameFilter
+                .NameFilter
+                .MatchCriterion
+                .SetValue(ENMatchCriterion.mcContains);
+            vendorQuery
+                .ORVendorListQuery
+                .VendorListFilter
+                .ORNameFilter
+                .NameFilter
+                .Name
+                .SetValue("Trust");
+        }
+        public static void handleResult(IMsgSetResponse res, List<string> names)
+        {
+            if(res == null)
+            {
+                return;
+            }
+            IResponseList resLst = res.ResponseList;
+            if(resLst == null || resLst.Count == 0)
+            {
+                return;
+            }
+            for(int i = 0; i < resLst.Count; i++)
+            {
+                IResponse r = resLst.GetAt(i);
+                if(r == null || r.Detail == null)
+                {
+                    continue;
+                }
+                IVendorRetList venRetLst = (IVendorRetList)r.Detail;
+                if(venRetLst.Count == 0)
+                {
+                    continue;
+                }
+                for(int j = 0; j < venRetLst.Count; j++)
+                {
+                    if(venRetLst.GetAt(j) == null || venRetLst.GetAt(j).Name == null)
+                    {
+                        continue;
+                    }
+                    names.Add(venRetLst.GetAt(j).Name.GetValue());
+                }
+            }
+        }
+    }
+}
---
+++ APIGetsSFData (1)/Controllers (1)/PostPOtoSF (1).cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Interop.QBFC15;
+using System;
+
+namespace APIGetsSFData.Controllers
+{
+    public class PostPOtoSF
+    {
+        public static async Task postData(Dictionary<string, salesReceipt> rqRec,
+            Dictionary<string, invoice> rqInv)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            string baseURL = "YOUR ORG URL INSTANCE";
+            string extention = "PATH TO API";
+            HttpResponseMessage res = await client.PostAsync(
+                baseURL + extention,
+                JsonContent.Create(
+                    new qbRequest
+                    {
+                        er = new requestContent
+                        {
+                            retrievedDictionary = rqRec,
+                            retrievedInvoice = rqInv
+                        }
+                    })); 
+            string response = await res.Content.ReadAsStringAsync();
+        }
+        public class qbRequest 
+        {
+            public requestContent er { get; set; }
+        }
+        public class requestContent
+        {
+            public Dictionary<string, salesReceipt> 
+                retrievedDictionary { get; set; }
+            public Dictionary<string, invoice> retrievedInvoice { get; set; }
+        }
+    }
+}
