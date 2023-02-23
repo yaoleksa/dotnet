c7c0107a6ce3972f82092c45e0db4fa883edd154
Oleksii <oleksii.y@peeklogic.com>
2023-02-23 22:09:23 +0200

    good luck

---
+++ APIGetsSFData (1)/Controllers (1)/PostPOtoSF (1).cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Interop.QBFC15;
+using System;
+
+namespace APIGetsSFData.Controllers
+{
+    public class PostPOtoSF
+    {
+        public static async Task postData(List<purchaseOrderRecord> recs)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            string baseURL = "YOUR ORG URL INSTANCE";
+            string extention = "PATH TO API";
+            HttpResponseMessage res = await client.PostAsync(
+                baseURL + extention,
+                JsonContent.Create(
+                    new qbRequest
+                    {
+                        er = new requestContent
+                        {
+                            retrievedList = recs
+                        }
+                    })); 
+            string response = await res.Content.ReadAsStringAsync();
+            Console.WriteLine("<<<<================>>>>>");
+            Console.WriteLine(response);
+        }
+        public class qbRequest 
+        {
+            public requestContent er { get; set; }
+        }
+        public class requestContent
+        {
+            public List<purchaseOrderRecord> retrievedList { get; set; }
+        }
+    }
+}
---
+++ APIGetsSFData (1)/Controllers (1)/UpdateSalesReceipt (1).cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using Interop.QBFC15;
+using System;
+
+namespace APIGetsSFData.Controllers
+{
+    public class UpdateSalesReceipt
+    {
+        public static void SalesReceiptMod(IMsgSetRequest modRq,
+            string customerName,
+            Dictionary<string, double> metalPrices,
+            salesReceipt value,
+            string sfId,
+            string date,
+            string qbId,
+            string editSequence
+            )
+        {
+            ISalesReceiptMod srmodRq = modRq.AppendSalesReceiptModRq();
+            srmodRq.TxnID.SetValue(qbId);
+            srmodRq.EditSequence.SetValue(editSequence);
+            srmodRq.CustomerRef.FullName.SetValue(customerName);
+            srmodRq.RefNumber.SetValue(sfId.Split('-')[1]);
+            DateTime dateSet = new DateTime();
+            DateTime currentDate = System.DateTime.Now;
+            if (date == "null")
+            {
+                dateSet = currentDate;
+            }
+            else if (currentDate
+                .Subtract(System
+                .Convert.ToDateTime(date))
+                .Days > 90)
+            {
+                dateSet = currentDate.AddDays(-90);
+            }
+            else
+            {
+                dateSet = System.Convert.ToDateTime(date);
+            }
+            srmodRq.TxnDate.SetValue(dateSet);
+            foreach(KeyValuePair<string, double> kvp in metalPrices)
+            {
+                IORSalesReceiptLineMod lineMod = srmodRq
+                    .ORSalesReceiptLineModList.Append();
+                salesReceiptLineItems val = new salesReceiptLineItems();
+                if (value.lineItems.ContainsKey(kvp.Key))
+                {
+                    value.lineItems.TryGetValue(kvp.Key, out val);
+                }
+                lineMod.SalesReceiptLineMod.TxnLineID.SetValue(val.id);
+                double rate = Math.Round(kvp.Value, 2);
+                lineMod.SalesReceiptLineMod.Amount.SetValue(rate);
+                lineMod.SalesReceiptLineMod.ORRatePriceLevel.Rate.SetValue(rate);
+            }
+        }
+    }
+}
