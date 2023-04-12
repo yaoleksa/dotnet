1e4a6a15bef4dbd97a9177b217360940dce96898
Oleksii <oleksii.y@peeklogic.com>
2023-04-12 21:30:22 +0300

    handle duplicate issue

---
+++ APIGetsSFData (1)/Controllers (1)/ApiCallSb (1).cs
@@ -0,0 +1,27 @@
+﻿using APIGetsSFData.Controllers;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System;
+
+public class ApiCallSb
+{
+    public static HashSet<purchaseOrderRecord> porLst = new
+        HashSet<purchaseOrderRecord>();
+    public static async Task getData()
+    {
+        HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+        string baseUrl =
+            "YOUR SANDBOX URL INSTANCE";
+        string extention = "PATH TO API";
+        client.DefaultRequestHeaders.Add("X-API-Key", 
+            "YOUR API KEY");
+        System.Threading.Tasks.Task<System.IO.Stream> tsk = client
+            .GetStreamAsync(baseUrl + extention);
+        HashSet<purchaseOrderRecord> response = await JsonSerializer
+            .DeserializeAsync<HashSet<purchaseOrderRecord>>(await tsk);
+        porLst = response;
+    }
+}
---
+++ APIGetsSFData (1)/Controllers (1)/PostPOtoSF (1).cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Interop.QBFC15;
+using System;
+
+namespace APIGetsSFData.Controllers
+{
+    public class PostPOtoSF
+    {
+        public static async Task postData(HashSet<purchaseOrderRecord> recs)
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            string baseURL = "YOUR ORG URL INSTANCE";
+            string extention = "PATH TO API";
+            HttpResponseMessage res = await client.PostAsync(
+                baseURL + extention,
+                JsonContent.Create(
+                    new qbRequest
+                    {
+                        er = new requestContent
+                        {
+                            retrievedList = recs
+                        }
+                    })); 
+            string response = await res.Content.ReadAsStringAsync();
+            Console.WriteLine("<<<<================>>>>>");
+            Console.WriteLine(response);
+        }
+        public class qbRequest 
+        {
+            public requestContent er { get; set; }
+        }
+        public class requestContent
+        {
+            public HashSet<purchaseOrderRecord> retrievedList { get; set; }
+        }
+    }
+}
